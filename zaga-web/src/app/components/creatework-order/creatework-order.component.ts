649aa61c6df1726ef99604076be385956b8a8419
Nemanja Dimsic <nemanjadimsic6@gmail.com>
2020-08-20 15:51:46 +0200

    create/workOrder component - static part

--- zaga-web/src/app/components/creatework-order/creatework-order.component.ts
+++ zaga-web/src/app/components/creatework-order/creatework-order.component.ts
@@ -6,10 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creatework-order.component.css']
 })
 export class CreateworkOrderComponent implements OnInit {
-
+  
   constructor() { }
+  workers = false;
+  machines = false;
+  materials = false;
 
+  tempJSON = [
+   {
+    "id": 1,
+    "name": "",
+    "start": "",
+    "end": "",
+    "field": "",
+    "culture": "",
+    "responsible": "",
+    "status": "",
+    "machines": [{
+      "id": 1,
+      "machine": "",
+      "worker": "",
+      "date": "",
+      "workPeriod": "",
+      "fuel": "",
+      "fuelType": "",
+      "storage": ""
+    }],
+    "workers": [{
+        "id": 1,
+        "worker": "",
+        "date": "",
+        "workPeriod": "",
+        "treatedArea": ""
+    }],
+    "materials": [{
+        "id": 1,
+        "name": "",
+        "quantity": "",
+        "unit": ""      
+    }]
+   },
+]
+  
   ngOnInit() {
+  }
+
+  expandWorkers() {
+    this.workers = !this.workers;
+  }
+  expandMachines() {
+    this.machines = !this.machines;
+  }
+  expandMaterials() {
+    this.materials = !this.materials;
   }
 
 }
