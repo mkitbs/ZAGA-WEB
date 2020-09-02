332906b6e08c7e1c91eeaf27f26e8673d722f7a9
Milos Vrgovic <milos.vrgovic@mksolutions.rs>
2020-09-02 11:41:32 +0200

    prethodna verzija, dodat api key

--- zaga-web/src/app/components/dashboard/dashboard.component.ts
+++ zaga-web/src/app/components/dashboard/dashboard.component.ts
@@ -716,8 +716,6 @@ export class DashboardComponent implements OnInit {
 
   //iscrtavanje tabli
   paths: any[]
-  require: any;
-  config = require('config');
  
   onTableClick(lat1, lng1, lat2, lng2, lat3, lng3, lat4, lng4, workerId){
     this.lat = lat1 + ((lat3 - lat1) / 2);
@@ -740,7 +738,6 @@ export class DashboardComponent implements OnInit {
     if(currentElement !== this.previousElement){
       this.renderer.setStyle(elementP, "background-color", "white");
     }
-    console.log(this.config.api_key)
   }
     
   ngOnInit() {
