42352d80aeee17a114f130a9a6e6ee16a88399bc
Nemanja Dimsic <nemanjadimsic6@gmail.com>
2020-09-21 12:10:52 +0200

    bugfix search autocomplete

--- zaga-web/src/app/pipes/search-employees-phone.pipe.ts
+++ zaga-web/src/app/pipes/search-employees-phone.pipe.ts
@@ -1,10 +1,10 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: "SearchEmployees",
+  name: "SearchEmployeesPhone",
   pure: false,
 })
-export class SearchEmployeesPipe implements PipeTransform {
+export class SearchEmployeesPhonePipe implements PipeTransform {
   transform(employees: any[], query): any {
     return employees.filter((employee) =>
       employee.name.toLowerCase().includes(query.toLowerCase())
