026d5539769cea325a8026f010ab26bb5569f4ce
Milos Vrgovic <milos.vrgovic@mksolutions.rs>
2020-12-01 16:01:14 +0100

    change environments

--- zaga-web/src/environments/environment.ts
+++ zaga-web/src/environments/environment.ts
@@ -2,19 +2,20 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-/*
+
 export const environment = {
   gatewayWorkOrderURL: "/proxy/work-order-service/",
   gatewayElasticURL: "/proxy/search-service/",
   production: false
 };
-*/
+
+/*
 export const environment = {
   gatewayWorkOrderURL: "http://localhost:8765/work-order-service/",
   gatewayElasticURL: "http://localhost:8765/search-service/",
   production: false,
 };
-
+*/
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
