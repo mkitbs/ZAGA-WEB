13ecb62aab7bdf7e81ac8bdba34e21c1e86766ff
Milos Vrgovic <milos.vrgovic@mksolutions.rs>
2020-12-07 12:48:31 +0100

    build error fix

--- zaga-web/src/environments/environment.prod.ts
+++ zaga-web/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   gatewayWorkOrderURL: "http://localhost:8765/work-order-service/",
   gatewayElasticURL: "http://localhost:8765/search-service/",
+  authURL: "http://localhost:8091/auth/",
+  userURL: "http://localhost:8091/user/",
   production: true,
 };
