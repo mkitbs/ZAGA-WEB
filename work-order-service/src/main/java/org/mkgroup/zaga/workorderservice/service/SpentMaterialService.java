6e84a097977b8813f81c359be65e443a6bebc531
Milos Vrgovic <m.vrgovic@vegait.rs>
2022-05-18 21:06:23 +0200

    commit1

--- work-order-service/src/main/java/org/mkgroup/zaga/workorderservice/service/SpentMaterialService.java
+++ work-order-service/src/main/java/org/mkgroup/zaga/workorderservice/service/SpentMaterialService.java
@@ -410,7 +410,7 @@ public class SpentMaterialService {
 		StringBuilder authEncodingString = new StringBuilder()
 				.append("LJKOMNENOVIC")
 				.append(":")
-				.append("Ljubica789");
+				.append("LjubKom987");
 		//Encoding Authorization String
 		String authHeader = Base64.getEncoder().encodeToString(
 	    		authEncodingString.toString().getBytes());
--- work-order-service/src/main/java/org/mkgroup/zaga/workorderservice/service/WorkerTimeTrackingService.java
+++ work-order-service/src/main/java/org/mkgroup/zaga/workorderservice/service/WorkerTimeTrackingService.java
@@ -269,7 +269,7 @@ public class WorkerTimeTrackingService {
 		StringBuilder authEncodingString = new StringBuilder()
 				.append("LJKOMNENOVIC")
 				.append(":")
-				.append("Ljubica789");
+				.append("LjubKom987");
 		//Encoding Authorization String
 		String authHeader = Base64.getEncoder().encodeToString(
 	    		authEncodingString.toString().getBytes());
